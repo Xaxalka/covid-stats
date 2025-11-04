9bfc1cb2218142574410a096f2e6b0bfbbc9aea0
Xaxalka <nikitastogov9@gmail.com>
2025-11-04 17:56:00 +0200

    last_changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import { Container, Nav, Tab } from 'react-bootstrap';
 import { CovidRecord } from './types/CovidData';
 import { fetchCovidData } from './utils/fetchData';
 import CountryFilter from './components/CountryFilter';
@@ -17,6 +17,7 @@ const App: React.FC = () => {
   const [valueField, setValueField] = useState('');
   const [minValue, setMinValue] = useState('');
   const [maxValue, setMaxValue] = useState('');
+  const [activeTab, setActiveTab] = useState('chart');
 
   useEffect(() => {
     fetchCovidData().then(setData).catch(console.error);
@@ -60,24 +61,39 @@ const App: React.FC = () => {
       countryFilter={countryFilter}
     />
 
-    {/* ← ВОТ ЗДЕСЬ ДИАГРАММА */}
-    <CovidChart 
-      data={data} 
-      countryFilter={countryFilter} 
-      dateFrom={dateFrom ? dateFrom.toISOString().split('T')[0] : undefined}
-      dateTo={dateTo ? dateTo.toISOString().split('T')[0] : undefined}
-    />
-
-    <TableView
-      data={data}
-      countryFilter={countryFilter}
-      valueField={valueField}
-      minValue={minValue === '' ? '' : Number(minValue)}
-      maxValue={maxValue === '' ? '' : Number(maxValue)}
-      onResetFilters={handleResetFilters}
-      dateFrom={dateFrom ? dateFrom.toISOString().split('T')[0] : ''}
-      dateTo={dateTo ? dateTo.toISOString().split('T')[0] : ''}
-    />
+    {/* Tab interface for switching between chart and table views */}
+    <Tab.Container id="covid-views" activeKey={activeTab} onSelect={(k) => setActiveTab(k || 'chart')}>
+      <Nav variant="tabs" className="mb-3 mt-3">
+        <Nav.Item>
+          <Nav.Link eventKey="chart">График</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="table">Таблица</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Tab.Content>
+        <Tab.Pane eventKey="chart">
+          <CovidChart 
+            data={data} 
+            countryFilter={countryFilter} 
+            dateFrom={dateFrom ? dateFrom.toISOString().split('T')[0] : undefined}
+            dateTo={dateTo ? dateTo.toISOString().split('T')[0] : undefined}
+          />
+        </Tab.Pane>
+        <Tab.Pane eventKey="table">
+          <TableView
+            data={data}
+            countryFilter={countryFilter}
+            valueField={valueField}
+            minValue={minValue === '' ? '' : Number(minValue)}
+            maxValue={maxValue === '' ? '' : Number(maxValue)}
+            onResetFilters={handleResetFilters}
+            dateFrom={dateFrom ? dateFrom.toISOString().split('T')[0] : ''}
+            dateTo={dateTo ? dateTo.toISOString().split('T')[0] : ''}
+          />
+        </Tab.Pane>
+      </Tab.Content>
+    </Tab.Container>
   </Container>
   );
 };
